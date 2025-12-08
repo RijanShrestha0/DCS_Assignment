20ab9a12980bb3421fc90b1d8bf089aaed9e67fa
Rijan Shrestha <8daudionp20@gmail.com>
2025-12-08 12:40:32 +0545

    week 6 Assignment

--- week6/algorithm/bubblesort.cpp
+++ week6/algorithm/bubblesort.cpp
@@ -3,21 +3,11 @@ using namespace std;
 
 void bubblesort(int arr[], int n) {
     for (int i = 0; i < n - 1; i++) {
-        cout << "Pass " << i + 1 << ": ";
-        for (int k = 0; k < n; k++) {
-            cout << arr[k] << " ";
-        }
-        cout << endl;
         for (int j = 0; j < n - 1 - i; j++) {
             if (arr[j] > arr[j + 1]) {
                 int temp = arr[j];
                 arr[j] = arr[j + 1];
                 arr[j + 1] = temp;
-                cout << "  Swapped " << arr[j+1] << " and " << arr[j] << ": ";
-                for (int k = 0; k < n; k++) {
-                    cout << arr[k] << " ";
-                }
-                cout << endl;
             }
         }
     }
